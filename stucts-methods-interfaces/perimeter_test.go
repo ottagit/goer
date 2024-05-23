aee83bef1877ff9bf0903822de373ae1f27f8214
ottagit <ottaosawa@gmail.com>
2024-05-23 15:55:28 +0300

    decouple the helper function from  concrete types
    Here, we have used an interface to declare onnly what is needed.
    This is key in software design.

--- stucts-methods-interfaces/perimeter_test.go
+++ stucts-methods-interfaces/perimeter_test.go
@@ -15,23 +15,23 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{10.0, 5.0}
-		got := rectangle.Area()
-		want := 50.0
 
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
 		if got != want {
 			t.Errorf("got %g want %g", got, want)
 		}
+	}
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 5.0}
+		want := 50.0
+		checkArea(t, rectangle, want)
 	})
 
 	t.Run("circles", func(t *testing.T) {
 		circle := Circle{10}
-		got := circle.Area()
 		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+		checkArea(t, circle, want)
 	})
 }
